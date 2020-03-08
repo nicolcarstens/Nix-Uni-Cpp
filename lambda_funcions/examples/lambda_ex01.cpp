f270d74a2a280bf6ddf11912baad0ec760eb35b7
Nicol Carstens <nicolcarstens@gmail.com>
2020-03-08 12:51:04 +0100

    More Lamdba and Threads

--- lambda_funcions/examples/lambda_ex01.cpp
+++ lambda_funcions/examples/lambda_ex01.cpp
@@ -1,5 +1,20 @@
+//
 // Simple Lambda function examples
+// run >> g++ --std=c++11 lambda_ex01.cpp 
+// 'auto' type specifier is a C++11 extension [-Wc++11-extensions]
 // Copyright: Udacity 2020
+//
+// A Lambda is a function object (a "functor"), so it has a type and 
+// can be stored and passed around. Its result object is called a 
+// "closure", which can be called using the operator ().
+//
+// A lambda formally consists of three parts: 
+//  => a capture list []
+//  => a parameter list () 
+//  => and a main part {}
+// ... which contains the code to be executed when the Lambda is called.
+// Note that in principal all parts could be empty.
+//
 
 #include <iostream>
 
